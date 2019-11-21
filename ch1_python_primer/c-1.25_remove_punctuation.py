ad7b90bdb79a5752d8c2ab2e9d2907b94754cc04
Gary Chen <g_chen8@mail.fhsu.edu>
2019-11-22 00:27:17 +0800

    Updated the answer to C-1.25

--- ch1_python_primer/c-1.25_remove_punctuation.py
+++ ch1_python_primer/c-1.25_remove_punctuation.py
@@ -1,5 +1,21 @@
 """
-Write a short Python function that takes a strings, representing 
+C-1.25 Write a short Python function that takes a strings, representing 
 a sentence, and returns a copy of the string with all punctuation removed. 
 For example, if given the string "Let s try, Mike.", this function would return "Lets try Mike".
 """
+def remove_punctuation(data):
+    new_str_list = []
+    alphabets = [chr(char) for char in range(ord('a'), ord('z')+1)]
+    whitespace = ' '
+    for char in data:
+        if char.lower() in alphabets or char.lower() in whitespace:
+            new_str_list.append(char)
+    return ''.join(new_str_list)
+
+data_str = "What's wrong?!! Nothing wrong!!!!"
+print("The sentence:\t {}".format(data_str))
+print("New sentence:\t {}".format(remove_punctuation(data_str)))
+
+# output:
+# The sentence:	 What's wrong?!! Nothing wrong!!!!
+# New sentence:	 Whats wrong Nothing wrong
