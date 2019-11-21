f50da033f992f37a22bbd5fdb5852d5d4f24dc45
Gary Chen <g_chen8@mail.fhsu.edu>
2019-11-21 23:10:31 +0800

    Update C-1.24

--- ch1_python_primer.py
+++ ch1_python_primer.py
@@ -1,1 +1,20 @@
 
+
+"""
+C-1.24 Write a short Python function that counts the number of vowels in a given
+character string.
+"""
+def count_vowel(data):
+    count = 0
+    vowels = 'aeiouAEIOU'
+    for char in data:
+        if char in vovwels:
+            count += 1
+    return count
+
+data_str = 'Idea of Life As a Gift'
+
+print("The sentence '{}' has {} vowels.".format(data_str, count_vowel(data_str)))
+
+# output: 
+# The sentence 'Idea of Life As a Gift' has 9 vowels.
